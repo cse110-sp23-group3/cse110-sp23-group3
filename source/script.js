56d453eba137b2af19bede467583a042778dabb7
henryzhang03 <henryzhang1719@gmail.com>
2023-05-20 16:49:52 -0700

    test JSDoc

--- source/script.js
+++ source/script.js
@@ -1,6 +1,20 @@
+/**
+ * This is a simple script that checks the value of x.
+ */
+
+// This is the value we'll be checking.
 const x = 5;
-if (x === 5) {
-  console.log('x is 5');
-} else {
-  console.log('x is not 5');
+
+/**
+ * This function logs a message depending on the value of x.
+ * @param {number} x - The value to check.
+ */
+function checkValue(x) {
+  if (x === 5) {
+    console.log('x is 5');
+  } else {
+    console.log('x is not 5');
+  }
 }
+
+checkValue(x);
