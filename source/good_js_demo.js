0bb472c929cac3a321096aa30ebb6e165b4acb5d
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-05-20 23:46:15 -0700

    Test CI pipeline

---
+++ source/good_js_demo.js
@@ -0,0 +1,14 @@
+function messAroundWithJavascript () {
+  let thisVariableIsFalse = true
+  if (thisVariableIsFalse) {
+      console.log('There is a paradox in this code.');
+  } else {
+    console.log('There is still a paradox in this code')
+  }
+}
+
+console.log('running');
+for (let i = 0; i < 5; i++) {
+  console.log(`i ${i}`);
+  messAroundWithJavascript();
+}
