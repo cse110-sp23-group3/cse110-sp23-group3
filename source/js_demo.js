2fc644c8e8424bcd58acccf4329c7b3f920ddf6b
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-05-21 00:29:48 -0700

    fixed js_demo.js

--- source/js_demo.js
+++ source/js_demo.js
@@ -1,16 +1,18 @@
-function messAroundWithJavascript ()
-{
-                        let unusedVariable = `This variable is not used in the code.`
-                        let thisVariableIsFalse = true
-                        if(thisVariableIsFalse) {
-                            console.log("There is a paradox in this code.");
-                        }
-                        else
-                            console.log("There is still a paradox in this code")
-}
-
-console.log('running');
-for (let i = 0 ;i < 5 ;i++) {
-    console.log('i ' + i);
-    messAroundWithJavascript ();
-}
+/**
+ * This function is used to demo the CI pipeline. It just does random stuff.
+ */
+function messAroundWithJavascript() {
+    let thisVariableIsFalse = true;
+    if (thisVariableIsFalse) {
+      console.log('There is a paradox in this code.');
+      thisVariableIsFalse = false;
+    } else {
+      console.log('There is still a paradox in this code');
+    }
+  }
+  
+  console.log('running');
+  for (let i = 0; i < 5; i++) {
+    console.log(`i ${i}`);
+    messAroundWithJavascript();
+  }
