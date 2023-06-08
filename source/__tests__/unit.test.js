d289001889314502deedba428880744f80d04d87
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-06-08 00:10:51 -0700

    fix: add sample unit test

--- source/__tests__/unit.test.js
+++ source/__tests__/unit.test.js
@@ -1,7 +1,11 @@
-const functions = require('../assets/scripts/chat.js');
+/**
+ * @jest-environment jsdom
+ */
 
-test('Save Array To History', () => {
-  functions.saveToHistory([]);
+const functions = require('../assets/scripts/historyHelpers');
+
+test('Save To History', () => {
+  functions.saveToHistory([1, 2, 3, 4], 1234);
   const palmReadings = JSON.parse(window.localStorage.getItem('palmReadings'));
-  expect(palmReadings.values().includes([])).toBeTruthy();
+  expect(palmReadings[1234]).toEqual([1, 2, 3, 4]);
 });
