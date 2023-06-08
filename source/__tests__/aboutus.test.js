ed87d7604600175f94be757fa7499c7e37a9e6ad
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-06-07 23:19:03 -0700

    fix: url too long in test

--- source/__tests__/aboutus.test.js
+++ source/__tests__/aboutus.test.js
@@ -1,7 +1,9 @@
 describe('Navigation To About Page', () => {
   // visit page TODO: update to the correct URL
   beforeAll(async () => {
-    await page.goto('https://cse110-sp23-group3.github.io/cse110-sp23-group3/source/index.html');
+    await page.goto('
+      https://cse110-sp23-group3.github.io/cse110-sp23-group3/source/index.html'
+    );
   });
 
   it('Check for existence of About Us button', async () => {
