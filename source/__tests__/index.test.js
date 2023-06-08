321d4e5e16ef41d529a5a7b830a954be55eea9a5
Anh Pham <phuanh004@gmail.com>
2023-06-08 14:08:02 -0700

    fix: highlight TODO

--- source/__tests__/index.test.js
+++ source/__tests__/index.test.js
@@ -1,6 +1,7 @@
 describe('Navigation To About Page', () => {
-  // visit page TODO: update to the correct URL
+  // visit page
   beforeAll(async () => {
+    // TODO: update to the correct URL
     await page.goto(
       'https://cse110-sp23-group3.github.io/cse110-sp23-group3/source/index.html'
     );
