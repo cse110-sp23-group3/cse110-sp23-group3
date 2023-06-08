c250d8bcbd7304a1aa9565d16cde7804e032bf19
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-06-07 23:22:13 -0700

    fix: string quote in wrong place

--- source/__tests__/aboutus.test.js
+++ source/__tests__/aboutus.test.js
@@ -1,8 +1,8 @@
 describe('Navigation To About Page', () => {
   // visit page TODO: update to the correct URL
   beforeAll(async () => {
-    await page.goto('
-      https://cse110-sp23-group3.github.io/cse110-sp23-group3/source/index.html'
+    await page.goto(
+      'https://cse110-sp23-group3.github.io/cse110-sp23-group3/source/index.html'
     );
   });
 
