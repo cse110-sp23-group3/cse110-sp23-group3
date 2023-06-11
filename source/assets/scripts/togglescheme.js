356d71947302b8ed5171a7cda869075eaa6d8fa3
henryzhang03 <henryzhang1719@gmail.com>
2023-06-11 15:51:48 -0700

    fix: add JSDoc to togglesheme.js

--- source/assets/scripts/togglescheme.js
+++ source/assets/scripts/togglescheme.js
@@ -1,9 +1,17 @@
+/**
+ * A constant that defines the 'data-theme' attribute.
+ * @constant
+ * @type {string}
+ */
 const DATA_THEME = 'data-theme';
 
 /**
- * Detect the user's preferred color scheme
- * and set the theme accordingly
- * @see https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/
+ * Function that detects the user's preferred color scheme (dark or light)
+ * and applies it as the theme to the document element. It also updates the state of
+ * the color scheme toggle button accordingly.
+ * @function
+ * @returns {void}
+ * @see {@link https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/}
  */
 function detectScheme() {
   const scheme = window.matchMedia('(prefers-color-scheme: dark)').matches
@@ -18,7 +26,11 @@ function detectScheme() {
 }
 
 /**
- * Toggle the color scheme
+ * Function that toggles the color scheme between dark and light when the color scheme
+ * toggle button is clicked. The selected scheme is then set as the 'data-theme' attribute
+ * of the document element and stored in the browser's local storage.
+ * @function
+ * @returns {void}
  */
 function toggleScheme() {
   const toggle = document.querySelector('#scheme-toggle');
@@ -33,7 +45,12 @@ function toggleScheme() {
   });
 }
 
-// DOM ready
+/**
+ * Adds an event listener to the document to ensure that the `detectScheme` and `toggleScheme`
+ * functions are called after the Document Object Model (DOM) has fully loaded.
+ * @function
+ * @returns {void}
+ */
 document.addEventListener('DOMContentLoaded', () => {
   detectScheme();
   toggleScheme();
