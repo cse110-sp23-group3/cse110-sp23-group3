d4c57f806ea560707a592a0eda7c452718be7db0
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-06-11 13:12:39 -0700

    fix: move tests that require mock to other file

--- source/__tests__/mock.unit.test.js
+++ source/__tests__/mock.unit.test.js
@@ -65,3 +65,50 @@ describe('rebuildChat', () => {
   });
 });
 
+describe('Add Message To Chat', () => {
+  // Here we are using Jest's mock functions to simulate the DOM functions
+  // createElement and appendChild are replaced by Jest's mock functions that don't do anything
+  // but they keep track of how many times they're called and with what arguments
+  document.createElement = jest.fn();
+  document.appendChild = jest.fn();
+  //const mockElement = document.createElement('div');
+  //const addSpy = jest.spyOn(mockElement.classList, 'add');
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Should create and append a message element', () => {
+    addMessageToChat('Test message', false);
+    // we're asserting that createElement was called once to create a 'div'
+    expect(document.createElement).toHaveBeenCalledTimes(1);
+    expect(document.createElement).toHaveBeenCalledWith('div');
+    // we're asserting that appendChild was called once (to append the message to the chat)
+    expect(document.appendChild).toHaveBeenCalledTimes(1);
+    //expect(addSpy).toHaveBeenCalledWith('chat-message');
+  });
+});
+
+
+describe('Clear Chat', () => {
+  document.getElementById = jest.fn(() => ({
+    innerHTML: 'not empty',
+  }));
+
+  it('Should clear chat', () => {
+    clearChat();
+    expect(document.getElementById).toHaveBeenCalledWith('chat-messages');
+    expect(document.getElementById().innerHTML).toBe('');
+  });
+});
+
+describe('Check if Ended', () => {
+  it('check to see that endedSession matches function return', () => {
+    global.endedSession = true;
+    const checkIfEndedOutput = checkIfEnded();
+    expect(checkIfEndedOutput).toBeTruthy();
+  });
+
+  it('check to see that endedSession matches function return', () => {
+    expect(checkIfEnded()).toThrow(new ReferenceError());
+  });
+});
